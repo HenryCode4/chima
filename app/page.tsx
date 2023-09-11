658a19cb1e54a2b029eaf4cc3d84e869eee88855
HenryCode4 <henrycnnamani4@gmail.com>
2023-09-11 09:58:28 -0700

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from 'next/image'
+import About from '@/components/About'
+import Intro from '@/components/Intro'
+import SectionDivider from '@/components/Section-divider'
+import Projects from '@/components/projects'
+
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>hello world</h1>
+    <main className="flex flex-col items-center px-4 ">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
     </main>
   )
 }
