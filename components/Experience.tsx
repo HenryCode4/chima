4b5d9bd23863731815bebe8d6b35dfe6e461ed72
HenryCode4 <henrycnnamani4@gmail.com>
2023-09-12 08:30:34 -0700

    dark mode added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -9,12 +9,12 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-// import { useTheme } from "@/context/theme-context";
+import { useTheme } from "@/context/theme-context";
 import SectionHeading from "./SectionHeading";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
-//   const { theme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -24,24 +24,24 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: "#f3f4f6",
-                  // theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+                background: 
+                  theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
-                  // theme === "light"
-                  //   ? "0.4rem solid #9ca3af"
-                  //   : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
               }}
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background: "white",
-                  // theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                background: 
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                 fontSize: "1.5rem",
               }}
             >
