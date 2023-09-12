7455fd23d27b27997fcf818364dc8767a15d8f00
HenryCode4 <henrycnnamani4@gmail.com>
2023-09-12 09:55:00 -0700

    all done

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -80,10 +80,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition shadow-lg"
-        //   onClick={() => {
-        //     setActiveSection("Contact");
-        //     setTimeOfLastClick(Date.now());
-        //   }}
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 shadow-lg"
-          href="/CV.pdf"
+          href="/chima.pdf"
           download
         >
           Download CV{" "}
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 shadow-lg"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/chima-nnamani-21a614230"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 shadow-lg"
-          href="https://github.com"
+          href="https://github.com/HenryCode4"
           target="_blank"
         >
           <FaGithubSquare />
